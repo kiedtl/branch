7d97e63899411b303da8f130739abe3282a385dc
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-11-08 21:16:57 -0500

    move tree glyphs to separate file

---
+++ src/glyf.rs
@@ -0,0 +1,20 @@
+// Unicode characters used to format
+// the tree view.
+#[derive(PartialEq, Debug)]
+pub enum TreeChars {
+    Entry,
+    Line,
+    LastEntry,
+    Blank,
+}
+
+impl TreeChars {
+    pub fn get(&self) -> &'static str {
+        match *self {
+            TreeChars::Entry        => "├── ",
+            TreeChars::Line         => "│   ",
+            TreeChars::LastEntry    => "└── ",
+            TreeChars::Blank        => "    ",
+        }
+    }
+}
